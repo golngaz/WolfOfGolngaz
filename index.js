2b818541994a536457c23c9f05a78c61ed00dfad
Gaël <galcantud@b2pweb.com>
2020-03-31 14:14:26 +0200

    test mp

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var bot = new Discord.Client();
 bot.on('message', function (message) {
     if (message.content === 'wog ping') {
         message.reply('pong');
+        message.author.send('coucou mon petit');
     }
 })
 
 
-bot.login('Njk0NDg4Nzg0MjkyNTQ0NTUy.XoMiDQ.ExMarytGzz0R2F8YQDNJ_NsbkEI');
+bot.login('Njk0NDg4Nzg0MjkyNTQ0NTUy.XoMvJA.NxlHywXdHGNeiB4CyGjQI9nn8mE');
